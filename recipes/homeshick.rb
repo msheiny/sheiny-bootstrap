5e259510df86e8bea1ed30eebdaf5684524d4bea
Michael Sheinberg <m.sheiny@gmail.com>
2014-07-05 16:37:14 -0400

    Lots and lots of debugging. Refactoring

--- recipes/homeshick.rb
+++ recipes/homeshick.rb
@@ -1,9 +1,9 @@
-hs_loc = "#{node['sheiny-bootstrap']['user_home']}/.homesick/repos/homeshick"
+hs_root = "#{node['sheiny-bootstrap']['user_home']}/.homesick"
+hs_loc = "#{hs_root}/repos/homeshick"
 castle = "#{node['sheiny-bootstrap']['my_repo_name']}"
 hs_clone = "#{node['sheiny-bootstrap']['my_repo_user']}/#{node['sheiny-bootstrap']['my_repo_name']}"
 
-[ "#{node['sheiny-bootstrap']['user_home']}/.homesick", 
-    "#{node['sheiny-bootstrap']['user_home']}/.homesick/repos" ].each do |path|
+[ "#{hs_root}", "#{hs_root}/repos" ].each do |path|
     directory path do 
         owner node["sheiny-bootstrap"]["user"]
         group node["sheiny-bootstrap"]["user"]
@@ -12,22 +12,20 @@ hs_clone = "#{node['sheiny-bootstrap']['my_repo_user']}/#{node['sheiny-bootstrap
     end
 end
 
-git "#{hs_loc}" do 
-    repository node["sheiny-bootstrap"]["homeshick"]
-    action :sync
-    user node["sheiny-bootstrap"]["user"]
-    group node["sheiny-bootstrap"]["user"]
-end
-
-
-if File.exists? "#{hs_loc}/#{castle}"
+if File.exists? "#{hs_root}/repos/#{castle}"
     execute "Update-homeshick" do 
-        command "#{hs_loc}/bin/homeshick pull -b #{castle}"
+        environment ({'HOME' => "#{node['sheiny-bootstrap']['user_home']}" })
+        command "#{hs_loc}/bin/homeshick pull -b #{castle} &&
+        #{hs_loc}/bin/homeshick link -b #{castle}"
+        user node["sheiny-bootstrap"]["user"]
         action :run
     end
 else
-    execute "Clone-homeshick" do 
-        command "#{hs_loc}/bin/homeshick clone -b #{hs_clone}"
+    execute "Pull-Down-homeshick" do 
+        environment ({'HOME' => "#{node['sheiny-bootstrap']['user_home']}" })
+        command "#{hs_loc}/bin/homeshick -f clone #{hs_clone} && 
+                #{hs_loc}/bin/homeshick -f link #{castle}"
+        user node["sheiny-bootstrap"]["user"]
         action :run
     end
 end
