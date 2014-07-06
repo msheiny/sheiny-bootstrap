7f4e69af3e53f8a2f4303fb5e22fdcced2b64637
Michael Sheinberg <m.sheiny@gmail.com>
2014-07-05 23:30:36 -0400

    Added vim airline bar and fonts

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -2,7 +2,8 @@
 home = node['sheiny-bootstrap']['user_home']
 
 
-[ "#{home}/.vim", "#{home}/.vim/bundle" ].each do |path|
+[ "#{home}/.vim", "#{home}/.vim/bundle", "#{home}/.fonts",
+        "#{home}/.fonts.conf.d"].each do |path|
     directory path do 
         owner node["sheiny-bootstrap"]["user"]
         group node["sheiny-bootstrap"]["user"]
@@ -22,4 +23,17 @@ execute "Vundle-Plugin-Install" do
     command "vim +PluginInstall +qall"
     user node["sheiny-bootstrap"]["user"]
     action :run
+end
+
+cookbook_file "PowerlineSymbols.otf" do
+    path "/usr/share/fonts/PowerlineSymbols.otf"
+end
+
+execute "Update-Font-Cache" do
+    command "fc-cache -vf"
+    action :run
+end
+
+cookbook_file "10-powerline-symbols.conf" do
+    path "/etc/fonts/conf.d/10-powerline-symbols.conf"
 end
